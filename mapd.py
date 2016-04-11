6ea999032717a6f5941f8aab78d6067152b2b137
Vladimir Scherbo <vscherbo@gmail.com>
2016-04-11 15:51:22 +0300

    отлажено чтение eeprom

--- mapd.py
+++ mapd.py
@@ -4,6 +4,7 @@
 import serial
 from ctypes import *
 from time import sleep
+from datetime import datetime
 
 maplib = cdll.LoadLibrary("libmapd_lib_dyn.so")
 
@@ -32,45 +33,60 @@ print "res_str=", str(res_str.raw)
 
 class MAP:
     map_answer = ''
+    eeprom = ''
     eeprom1 = ''
     eeprom2 = ''
     fd = -1
+    t_start = 0
+    t_fin = 0
+    Vhigh = 0
+    Vlow = 0
     buf = create_string_buffer(size_of_buffer)
     def __init__(self, port_name, baudrate, timeout):
         self.port = serial.Serial(port_name, baudrate=baudrate, timeout=timeout)
         self.fd = self.port.fileno()
     def send_command(self, command, addr, page):
+        self.t_start = datetime.now()
+        print 'start send_command=', self.t_start
         maplib.send_command(command, self.fd, addr, page, byref(self.buf))
+        self.t_fin = datetime.now()
+        print 'finish send_command=', self.t_fin
+        print "delta=", (self.t_fin - self.t_start).total_seconds()
     def read_answer(self):
         return maplib.read_answer(self.fd, byref(self.buf))
     def read_data(self):
         return maplib.read_data(self.fd)
     def read_eeprom(self):
         self.send_command(to_read, 0, 0xFF)
-        #self.eeprom1 = self.read_data()
         self.rc = self.read_answer()
         if 0 == self.rc:
-            self.eeprom1 = self.buf
+            #self.eeprom1 = self.buf[:0xFF]
+            self.eeprom = self.buf[:0xFF]
         else:            
             self.eeprom1 = ''
             print "rc_read_eeprom1=", self.rc
+            return False
         #
         self.send_command(to_read, 0x100, 0xFF)
         self.rc = self.read_answer()
         if 0 == self.rc:
-            self.eeprom2 = self.buf
+            # self.eeprom2 = self.buf[:0xFF]
+            # self.eeprom = self.eeprom1 + self.eeprom2
+            # self.eeprom = self.eeprom1 + self.buf[:0xFF]
+            self.eeprom += self.buf[:0xFF]
         else:            
             self.eeprom2 = ''
             print "rc_read_eeprom2=", self.rc
-        #self.eeprom2 = self.read_data()
+            return False
+        return True
 
 mapups = MAP('/dev/ttyUSB0', baudrate=19200, timeout=5)
 print "MAP created"
 #print mapups
 print "port=", mapups.port
-print "fd=", mapups.fd
-print "len(buf)=", len(mapups.buf)
-print "type(buf)=", type(mapups.buf)
+#print "fd=", mapups.fd
+#print "len(buf)=", len(mapups.buf)
+#print "type(buf)=", type(mapups.buf)
 
 """
 mapups.send_command(to_read, 0, 0xFF)
@@ -81,26 +97,19 @@ print "type(res.raw)=", type(res.raw)
 print "res=", repr(res.raw)
 """
 
-mapups.read_eeprom()
-H = ord(mapups.eeprom2[0x69]) + 100
-L = ord(mapups.eeprom2[0x6A]) + 100
-print "H=", H
-print "L=", L
-#print "type(mapups.eeprom2)=", type(mapups.eeprom2)
-print "mapups.eeprom1="
-print ''.join("{:02x}".format(ord(c)) for c in mapups.eeprom1)
-print "mapups.eeprom2="
-print ''.join("{:02x}".format(ord(c)) for c in mapups.eeprom2)
-
-"""
-rc_put = maplib.put_command(to_read, mapups.fd, 0, 0xFF, byref(mapups.buf))
-print "rc_put=", rc_put
-print "in_waiting=", mapups.port.in_waiting
-
-rc = maplib.read_answer(mapups.fd, byref(mapups.buf))
-print "rc=", rc
-if 0 == rc :
-    print ''.join("{:02x}".format(ord(c)) for c in mapups.buf)
+if mapups.read_eeprom():
+    H = ord(mapups.eeprom[0x169]) + 100
+    L = ord(mapups.eeprom[0x16A]) + 100
+    #print ''.join("{:02x}".format(ord(c)) for c in mapups.eeprom[0x169:0x16C])
+    print "H=", H
+    print "L=", L
+    """
+    print "mapups.eeprom1="
+    print ''.join("{:02x}".format(ord(c)) for c in mapups.eeprom1)
+    print "mapups.eeprom2="
+    print ''.join("{:02x}".format(ord(c)) for c in mapups.eeprom2)
+    """
+    print "mapups.eeprom="
+    print ''.join("{:02x}".format(ord(c)) for c in mapups.eeprom)
 else:
-    print "Error"
-"""    
+    print "Error read_eeprom()"
